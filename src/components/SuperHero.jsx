e49a175cc5ab56a9c46108f983b6902143acd4cb
Joan Kenedy <joankenedi9311@gmail.com>
2024-10-07 16:07:12 -0600

    super-hero-text

--- src/components/SuperHero.jsx
+++ src/components/SuperHero.jsx
@@ -26,9 +26,9 @@ export const SuperHero = ({ bgCompu, bgCel, textCategoria }) => {
                 : { backgroundImage: `url(${bgCompu})` }
             }
           >
-            <div className="phrase absolute left-0 top-0 w-full h-full flex justify-center items-end md:items-center ">
-              <div className="container-phrase mb-16 md:mb-0 ">
-                <h2 className="title-slider text-center text-white text-2xl  uppercase  md:text-5xl frase px-5">
+            <div className="phrase absolute left-0 top-0 w-full h-full flex justify-center items-center ">
+              <div className="container-phrase  ">
+                <h2 className="title-slider text-center  text-white text-3xl  uppercase  md:text-6xl frase px-5">
                   {textCategoria}
                 </h2>
               </div>
