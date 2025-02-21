90a1b15e14c46b401812f14f07ceaf660ef8ba02
Joan Kenedy <joankenedi9311@gmail.com>
2025-02-21 10:57:40 -0600

    promos-doctores-campaña

--- src/components/Promociones/Promocion.jsx
+++ src/components/Promociones/Promocion.jsx
@@ -1,6 +1,6 @@
 import Promo1 from "../../assets/promo-1.jpg";
 import Promo2 from "../../assets/promo-2.jpg";
-import Promo4 from "../../assets/promo-3.jpg";
+import Promo4 from "../../assets/promo-4.jpg";
 
 export const Promocion = () => {
   return (
@@ -9,26 +9,27 @@ export const Promocion = () => {
         <h2 className="text-2xl font-bold text-center uppercase md:text-3xl mb-3">
           Nuestras promociones
         </h2>
-        <div className="container-promos w-[90%] m-auto grid gap-3 md:grid-cols-3 md:max-w-6xl">
-          <figure>
-            <img
-              src={Promo4}
-              alt=""
-              className="max-w-[100%] md:hover:scale-95 duration-500"
-            />
-          </figure>
+        <div className="container-promos w-[90%] m-auto grid gap-3 md:grid-cols-3 md:gap-5 md:max-w-7xl">
           <figure>
             <img
               src={Promo1}
               alt=""
-              className="max-w-[100%] md:hover:scale-95 duration-500"
+              className="max-w-[100%] md:hover:scale-95 duration-500 shadow-gray-800 shadow-lg"
             />
           </figure>
           <figure>
             <img
+              src={Promo4}
+              alt=""
+              className="max-w-[100%] md:hover:scale-95 duration-500 shadow-gray-800 shadow-lg"
+            />
+          </figure>
+
+          <figure>
+            <img
               src={Promo2}
               alt=""
-              className="max-w-[100%] md:hover:scale-95 duration-500"
+              className="max-w-[100%] md:hover:scale-95 duration-500 shadow-gray-800 shadow-lg"
             />
           </figure>
         </div>
